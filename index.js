06b2345783a5f755c9ef5427a0d2aebbea1f50a3
maxvyr <maxvid30@gmail.com>
2022-05-07 17:00:56 +0200

    :sparkles: add inquiere

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-import { getCurrentDirectoryBase, fileExist } from './lib/file.js'
+#!/usr/bin/env node
+
+import { getCurrentDirectoryBase, fileExist, checkAllfile } from './lib/file.js'
 import { clear } from './lib/clear.js';
 import chalk from 'chalk';
 import figlet from 'figlet';
+import { selectedFiles } from './lib/inquirer_local.js';
 
 
 clear();
 
 console.log(
     chalk.yellow(
-        figlet.textSync('Choose Your Work Modules', { horizontalLayout: 'full' })
+        figlet.textSync('Choose Your Modules', { horizontalLayout: 'full' })
     )
 );
 
 if (fileExist('tengu-sensei.code-workspace')) {
-    console.log(chalk.bgCyan('Start selection'));
+    checkAllfile();
 } else {
     console.log(chalk.red('Code Workspaces not found!'));
     process.exit();
